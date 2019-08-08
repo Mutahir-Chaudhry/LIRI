82bf8dbb7d1b7669993e9ab2075559ea9758fa20
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-08-08 17:47:26 -0400

    Finalized all commands except do-what-it-says at this time. Have the bonus working with all new information being appended to the log.txt file without overwriting preexisting information.

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var fs = require("fs")
 // Import the keys.js file and store it in a variable
 var keys = require("./keys.js");
 
-//
+//Require moment.js for time conversions
 var moment = require('moment');
 moment().format();
 
@@ -45,96 +45,113 @@ switch (action) {
         break;
 }
 
-//Import Spotify API and store as variable
-var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify(keys.spotify);
-
-function spotifyThis() {
-    spotify.search({ type: 'track', query: value }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        for (i = 0; i < data.tracks.items.length; i++) {
-            var name = data.tracks.items[i].name
-            var artists = data.tracks.items[i].artists[0].name
-            var album = data.tracks.items[i].album.name
-            var url = data.tracks.items[i].external_urls.spotify
-
-            console.log("Name: " + name)
-            console.log("Artist(s): " + artists)
-            console.log("Album:" + album)
-            console.log("Link: " + url)
-
-            fs.appendFile("log.txt", "\n " + JSON.stringify(name), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-                else {
-                    console.log("Name Added!");
-                }
-            });
-            fs.appendFile("log.txt", ", " + JSON.stringify(artists), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-                else {
-                    console.log("Artists Added!");
-                }
-            });
-            fs.appendFile("log.txt", ", " + JSON.stringify(album), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-                else {
-                    console.log("Album Added!");
-                }
-            });
-            fs.appendFile("log.txt", ", " + JSON.stringify(url), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-                else {
-                    console.log("URL Added!");
-                }
-            });
-        }
-    })
-};
+//concert-this function
 function concertThis(){
     var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp"
 
     axios.get(queryUrl).then(
         function(response) {
+
             console.log(response.data[0])
+
             for (i = 0; i < response.data.length; i++) {
+
             var momentTime = moment(response.data[i].datetime).format('MM/DD/YYYY');
-            console.log("\nThis concert will be held on " + momentTime + " and the location of this concert is " + response.data[i].venue.name + ", located in the city of " + response.data[i].venue.city + ", " + response.data[i].venue.country + ".")
+            var name = response.data[i].venue.name;
+            var city = response.data[i].venue.city;
+            var country = response.data[i].venue.country;
+            
+            console.log("\nNew Concert!\nDate of concert: " + momentTime)
+            console.log("Venue name: " + name)
+            console.log("City: " + city)
+            console.log("Country: " + country)
+
+            fs.appendFile("log.txt", "\nNew Concert!\n This concert is on " + JSON.stringify(momentTime), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ", at " + JSON.stringify(name), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ", in the city of " + JSON.stringify(city), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ", " + JSON.stringify(country), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            
         }
     }).catch(function (error) {
             if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
+
                 console.log("---------------Data---------------");
                 console.log(error.response.data);
                 console.log("---------------Status---------------");
                 console.log(error.response.status);
                 console.log("---------------Status---------------");
                 console.log(error.response.headers);
+
             } else if (error.request) {
                 console.log(error.request);
+
             } else {
-                // Something happened in setting up the request that triggered an Error
+
                 console.log("Error", error.message);
             }
             console.log(error.config);
         })
 }
 
+//spotify-this-song function
+function spotifyThis() {
+    spotify.search({ type: 'track', query: value }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        for (i = 0; i < data.tracks.items.length; i++) {
+            
+            var name = data.tracks.items[i].name
+            var artists = data.tracks.items[i].artists[0].name
+            var album = data.tracks.items[i].album.name
+            var url = data.tracks.items[i].external_urls.spotify
+
+            console.log("\nNew Song!\nName: " + name)
+            console.log("Artist(s): " + artists)
+            console.log("Album:" + album)
+            console.log("Link: " + url)
+
+            fs.appendFile("log.txt", "\nNew Song!\n " + JSON.stringify(name), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". Created by " + JSON.stringify(artists), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". For the album " + JSON.stringify(album), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". Link to song: " + JSON.stringify(url), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    })
+};
+
+//movie-this function
 function movieThis() {
     // Then run a request with axios to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=6382e4d1";
@@ -142,25 +159,56 @@ function movieThis() {
     axios.get(queryUrl).then(
         function (response) {
             var title = response.data.Title;
-            console.log(title);
-
             var year = response.data.Year;
-            console.log("Year: " + year);
-
             var imdbRating = response.data.imdbRating;
-            console.log("imdbRating: " + imdbRating);
-
             var country = response.data.Country;
-            console.log("Country of Production: " + country);
-
             var language = response.data.Language;
-            console.log("Language:" + language);
-
             var plot = response.data.Plot;
-            console.log("Plot: " + plot);
-
             var actors = response.data.Actors;
+            
+            console.log("\nNew Movie!\n"+ title);
+            console.log("Year: " + year);
+            console.log("imdbRating: " + imdbRating);
+            console.log("Country of Production: " + country);
+            console.log("Language:" + language);
+            console.log("Plot: " + plot);
             console.log("Actors: " + actors);
+
+            fs.appendFile("log.txt", "\nNew Movie!\n " + JSON.stringify(title), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " came out in " + JSON.stringify(year), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". It is rated at  " + JSON.stringify(imdbRating), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ", and was filmed in " + JSON.stringify(country), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". It is in  " + JSON.stringify(language), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". It's plot is: " + JSON.stringify(plot), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", ". The actors in this film are " + JSON.stringify(actors), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         })
         .catch(function (error) {
             if (error.response) {
@@ -182,6 +230,7 @@ function movieThis() {
         })
 };
 
+//do-what-it-says function
 function doThis() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         // Then split it by commas (to make it more readable)
@@ -198,7 +247,7 @@ function doThis() {
         if (error) {
             return console.log(error);
         }else if (action === "spotify-this-song") {
-            //Havent figured this out yet
+        spotifyThis(track, "I Want it That Way")
         }
     });
 }
